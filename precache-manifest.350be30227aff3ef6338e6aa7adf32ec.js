99d9ead121387469165ac4105fe548fc54723939
pmthang1 <pmthang1@gmail.com>
2019-07-28 21:04:20 +0700

    Updates

---
+++ precache-manifest.350be30227aff3ef6338e6aa7adf32ec.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24a1d3fb8149635a506853a7eed06eb",
+    "url": "/blog-site/index.html"
+  },
+  {
+    "revision": "af0ef61fff9aa5179a1d",
+    "url": "/blog-site/static/js/2.0cdbd1e6.chunk.js"
+  },
+  {
+    "revision": "16f63ceb999a562c9312",
+    "url": "/blog-site/static/js/main.7db0ce80.chunk.js"
+  },
+  {
+    "revision": "2c93a6b4ad2e366be662",
+    "url": "/blog-site/static/js/runtime~main.1aaf2cb4.js"
+  }
+]);
